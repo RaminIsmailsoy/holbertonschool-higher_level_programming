96213afc770b8b024a00be8e36018791599adec1
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-26 19:17:24 +0000

    all

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+""" MyList class """
 
+
+class MyList(list):
+    """ Create MyList class """
+
+    def print_sorted(self):
+        """Print a list in sorted order"""
+        print(sorted(self))
