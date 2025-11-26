6b954b73c60915d7d7e8f7d56dc1dbacf33040b5
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-26 06:47:25 +0000

    all

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" This module defines a Rectangle class with width and height """
+"""
+This module defines a Rectangle class with width and height,
+and methods to compute area and perimeter.
+"""
+
 
 class Rectangle:
     """Class representing rectangle"""
