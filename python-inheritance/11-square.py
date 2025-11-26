23fa9aa90875212eafcaa2a00372ffc6ec1a372a
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-26 20:21:17 +0000

    all

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+''' Task 11. Square #2 '''
+Rectangle = __import__('9-rectangle').Rectangle
 
+
+class Square(Rectangle):
+    ''' Class Square that uses Rectangle  '''
+
+    def __init__(self, size):
+        ''' Initialize a new square.
+
+        Arguments are:
+            size (int): The size of the new square.
+        '''
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
