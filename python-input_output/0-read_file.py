0e7cf8ee5aa9cff14ec98c80fd90b06deec983f6
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-27 14:50:05 +0000

    all

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-    ''' Write a function that reads a text file (UTF8) and prints it to stdout:'''
+    ''' Write a function that reads a text file (UTF8)'''
+
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
