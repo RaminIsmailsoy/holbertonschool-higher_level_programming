1da5493d1f0b59a78c381331e65204b7904af154
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-27 14:55:43 +0000

    all

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+''' Append to a file '''
+
+def append_write(filename="", text=""):
+    ''' Appends a string to a UTF-8 text file and returns
+        the number of characters added.'''
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
+
