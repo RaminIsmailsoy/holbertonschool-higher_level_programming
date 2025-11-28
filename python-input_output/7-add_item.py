c562f3e5f952a55bfefc84309e9b921646fe1fa1
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 19:08:02 +0000

    all

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,22 +2,17 @@
 ''' Task 7. Write a script that adds all arguments to a Python list, and then save them to a file '''
 
 
-from os import path
-''' The Operating System interface module. It provides a way of interacting with the operating system.
-Things like reading or writing files, creating directories, and handling environment variables.'''
+import sys
 
-from sys import argv
-''' The System module. It provides access to system-specific parameters and functions '''
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
-
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
-
-save_to_json_file(obj_json_file, 'add_item.json')
+    try:
+        item = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")
