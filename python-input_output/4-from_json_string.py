81b571e004e5fff0a1f3f05484a0b2338be58a0c
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 18:10:49 +0000

    all

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+
 ''' 4. From JSON string to Object '''
 
 
 from json import loads
 
+''' Import loads function from Pythons built-in json modul '''
 
 def from_json_string(my_str):
+
+    ''' Define a function '''
+
     return loads(my_str)
