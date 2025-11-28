09f67eb5ef2716a6dc81a45c4b4721078582d4c9
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 18:00:19 +0000

    all

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,7 +5,7 @@
 
 from json import dumps
 
-    ''' Import dumps function from json module. Python has a built-in module called json '''
+''' Import dumps function from json module. Python has a built-in module called json '''
 
 def to_json_string(my_obj):
     return dumps(my_obj)
