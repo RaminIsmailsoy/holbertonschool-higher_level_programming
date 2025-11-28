ed353df1bf4338c681ef5323d01d1ba2edc29733
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 18:57:44 +0000

    all

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-''' Task 7. Load, add, save '''
+''' Task 7. Write a script that adds all arguments to a Python list, and then save them to a file '''
 
 
 from os import path
+''' The Operating System interface module. It provides a way of interacting with the operating system.
+Things like reading or writing files, creating directories, and handling environment variables.'''
+
 from sys import argv
+''' The System module. It provides access to system-specific parameters and functions '''
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
