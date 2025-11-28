046310d1bed46d8244fa156fda90d23b5874ca6e
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 18:48:40 +0000

    all

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
 ''' Task 6. Create object from a JSON file '''
+''' Write a function that creates an Object from a JSON file '''
 
 
 from json import loads
 
+''' Imports the loads function from the json module.
+loads function is used for deserialization.
+Converting a JSON string back into a Python object '''
+
 
 def load_from_json_file(filename):
+
+    ''' Define a function called load_from_json_file '''
+
     with open(filename, encoding='utf-8') as f:
+
+        ''' Read the entire JSON string content from the file '''
         return loads(f.read())
