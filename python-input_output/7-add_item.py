93621575def2a9e3fda7ad35a3f86de52e03e9d4
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 19:04:00 +0000

    all

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 ''' Saves to a file'''
 
-load_from_json_file = __import__('86-load_from_json_fil').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_fil').load_from_json_file
 
 if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
