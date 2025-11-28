e573f5880471bf1de2c78aa8d49e000c46b9e367
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 19:02:32 +0000

    all

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,6 +10,8 @@ from sys import argv
 ''' The System module. It provides access to system-specific parameters and functions '''
 
 save_to_json_file = __import__(''5-save_to_json_file).save_to_json_file
+''' Saves to a file'''
+
 load_from_json_file = __import__('86-load_from_json_fil').load_from_json_file
 
 if path.exists('add_item.json'):
