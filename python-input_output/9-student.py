751e4039f72eb91ce58a7d591ea736de6840e571
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-28 19:18:16 +0000

    all

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,12 +7,13 @@ Public instance attributes: first_name, last_name, age
 
 class Student:
 
-    ''' Defines a new class called Student. A class is a blueprint for creating objects. '''
+    ''' Defines a new class called Student.
+    A class is a blueprint for creating objects. '''
 
     def __init__(self, first_name, last_name, age):
 
         ''' Initialize Student class '''
-        ''' This method is automatically called when a new Student object is created. '''
+        ''' This method is automatically called when a new Student object is created'''
         ''' self  refers to the current object being created .'''
 
         self.first_name = first_name
@@ -20,8 +21,8 @@ class Student:
         self.age = age
 
     def to_json(self):
-        
-        ''' to_json is a method that returns a dictionary representation of the object '''
+
+        ''' to_json is a method that returns a dictionary representation of the object'''
         ''' It can be converted to JSON. '''
-        
+
         return self.__dict__
