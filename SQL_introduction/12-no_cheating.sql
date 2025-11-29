32693aa2e01e338e155cfedd68371284609eac47
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 07:31:39 +0000

    all

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Write a script that updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bobs id value, only the name field
+UPDATE SET second_table score=10 WHERE name="Bob";
