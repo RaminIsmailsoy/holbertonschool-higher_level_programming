7fe4033265760017cc020f4de458b9a7ef8cfcee
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 06:18:45 +0000

    all

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- All fields should be printed. The database name will be passed as an argument
+SELECT * FROM first_table;
