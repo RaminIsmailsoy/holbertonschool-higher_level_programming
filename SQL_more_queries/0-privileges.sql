4361f20388f93ebb9a86c6368aa14cef652c62ee
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 11:30:37 +0000

    all

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
-mysql -uroot -e "SHOW GRANTS FOR 'user_0d_1'@'localhost'"
-mysql -uroot -e "SHOW GRANTS FOR 'user_0d_2'@'localhost'"
+echo "SHOW GRANTS FOR 'user_0d_1'@'localhost'" | mysql -hlocalhost -uroot
+echo "SHOW GRANTS FOR 'user_0d_2'@'localhost'" | mysql -hlocalhost -uroot
