a54afe6b13f18362daca25076efaaf8253d79734
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 19:01:22 +0000

    all

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -3,6 +3,8 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name Results must be sorted in ascending order by the genre name
 -- You can use a maximum of two SELECT statement The database name will be passed as an argument of the mysql command
-SELECT name FROM tv_genres
-WHERE id NOT IN (SELECT genre_id FROM tv_show_genres WHERE show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter'))
-ORDER BY name ASC;
+SELECT tv_genres.name FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+AND tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_genres.name ASC;
