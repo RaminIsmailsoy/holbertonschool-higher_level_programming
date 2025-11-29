d772d0e715c62903fea334f92d5a7d811010ff17
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 19:06:44 +0000

    all

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -4,6 +4,8 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title AS title, SUM(ratings.rating) AS rating FROM tv_shows
-JOIN ratings ON tv_shows.id = ratings.show_id GROUP BY tv_shows.title
+SELECT tv_shows.title AS title, SUM(ratings.rating) AS rating
+FROM tv_shows
+JOIN ratings ON tv_shows.id = ratings.show_id
+GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
