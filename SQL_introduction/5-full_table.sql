c22ed71bd5ed49543b064c8545211f27399805d0
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 04:52:22 +0000

    all

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+SHOW TABLES first_table;
