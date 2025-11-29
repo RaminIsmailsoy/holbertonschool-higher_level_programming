00555863b0e81b23eed70d45c0b6261b3802359d
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-29 17:18:33 +0000

    all

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-
+ELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows WHERE COLUMN="tv_shows.title" and "tv_show_genres.genre_id" ORDER BY tv_shows.title;
