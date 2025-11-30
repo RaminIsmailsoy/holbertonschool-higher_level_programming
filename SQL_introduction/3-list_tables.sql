1febb7b20722b9e944ed78240de97d7cadc9676b
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-30 19:03:46 +0000

    all

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,5 @@
 -- Write a script that lists all the tables of a database in your MySQL server
 -- The database name will be passed as argument
+-- echo "SHOW TABLES;" | mysql -hlocalhost -uroot database_name
+-- cat list_tables.sql | mysql -hlocalhost database_name
 SHOW TABLES;
